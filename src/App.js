d54ce73738726a00fbae359f8581ea986c265642
ViDA_TG <victorademiju.t@gmail.com>
2023-01-07 23:47:23 +0100

    New commmit

--- src/App.js
+++ src/App.js
@@ -1,2 +1,24 @@
 import * as React from 'react';
 import Map, {Marker} from 'react-map-gl';
+
+function App() {
+  const [viewState, setViewState] = React.useState({
+    longitude: 2.294694,
+    latitude: 48.858093,
+    zoom: 4
+  });
+
+  return <Map
+    {...viewState}
+    onMove={evt => setViewState(evt.viewState)}
+    style={{width: "100vw", height: "100vh"}}
+    mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+    mapStyle="mapbox://styles/mapbox/streets-v9"
+  >
+  <Marker longitude={2.294694} latitude={48.858093} anchor="bottom" >
+    <div>Yoffffffffffffffffffffffffffffffffffffffffffffffff</div>
+  </Marker>
+  </Map>;
+}
+
+export default App;
